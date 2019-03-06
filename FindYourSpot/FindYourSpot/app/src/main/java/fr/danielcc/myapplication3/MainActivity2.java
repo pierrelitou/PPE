fe93ed7e364dbfbef4c37346a4c2ee89634d08c2
danielclaroc <daniel.claro-carvalho@edu.ece.fr>
2019-03-06 11:40:31 +0100

    Boutons en Bas + debut DB sur MapsActivity

--- FindYourSpot/FindYourSpot/app/src/main/java/fr/danielcc/myapplication3/MainActivity2.java
+++ FindYourSpot/FindYourSpot/app/src/main/java/fr/danielcc/myapplication3/MainActivity2.java
@@ -20,7 +20,7 @@ import java.util.HashMap;
 public class MainActivity2 extends AppCompatActivity{
 
     //Link to database
-    public static final String URL_GET_ALL = "http://192.168.0.14/findyourspot/GetActivities.php";
+    public static final String URL_GET_ALL = "http://192.168.43.192/findyourspot/GetActivities.php";
 
     //JSON Tagsvity
     public static final String TAG_JSON_ARRAY="result";
--- FindYourSpot/FindYourSpot/app/src/main/java/fr/danielcc/myapplication3/MapsActivity.java
+++ FindYourSpot/FindYourSpot/app/src/main/java/fr/danielcc/myapplication3/MapsActivity.java
@@ -1,6 +1,8 @@
 package fr.danielcc.myapplication3;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,10 +15,25 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    //Link to database
+    public static final String URL_GET_ALL = "http://192.168.43.192/findyourspot/GetGPSPositions.php";
+
+    //JSON Tagsvity
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_LAT = "Lat";
+    public static final String TAG_LNG = "Lng";
+
+    private String JSON_STRING;
 
     private GoogleMap mMap;
     private Button activity;
@@ -28,8 +45,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
 
+        //listView.setOnItemClickListener(this);
+        getJSON();
         this.activity = (Button) findViewById(R.id.ACTIVITES);
-
 
         activity.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,24 +94,79 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+    private ArrayList showEmployee() {
+        JSONObject jsonObject = null;
+        ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
+        try {
+            jsonObject = new JSONObject(JSON_STRING);
+            JSONArray result = jsonObject.getJSONArray(TAG_JSON_ARRAY);
 
+            for (int i = 0; i < result.length(); i++) {
+                JSONObject jo = result.getJSONObject(i);
+                String Lat = jo.getString(TAG_LAT);
+                String Lng = jo.getString(TAG_LNG);
+                //          String lienimg = jo.getString(TAG_IMG);
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+                HashMap<String, String> employees = new HashMap<>();
+
+                employees.put(TAG_LAT, Lat);
+                employees.put(TAG_LNG, Lng);
+
+                list.add(employees);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+        private void getJSON(){
+            class GetJSON extends AsyncTask<Void,Void,String> {
+
+                ProgressDialog loading;
+                @Override
+                protected void onPreExecute() {
+                    super.onPreExecute();
+                    loading = ProgressDialog.show(MapsActivity.this,"Fetching Data","Wait...",false,false);
+                }
+
+                @Override
+                protected void onPostExecute(String s) {
+                    super.onPostExecute(s);
+                    loading.dismiss();
+                    JSON_STRING = s;
+                    showEmployee();
+                }
+
+                @Override
+                protected String doInBackground(Void... params) {
+                    RequestHandler rh = new RequestHandler();
+                    String s = rh.sendGetRequest(URL_GET_ALL);
+                    return s;
+                }
+            }
+            GetJSON gj = new GetJSON();
+            gj.execute();
+        }
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera. In this case,
+         * we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to install
+         * it inside the SupportMapFragment. This method will only be triggered once the user has
+         * installed Google Play services and returned to the app.
+         */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        //ArrayList listpositions = showEmployee();
 
-        // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        // Add a marker in Sydney and move the camera
+
     }
 }
