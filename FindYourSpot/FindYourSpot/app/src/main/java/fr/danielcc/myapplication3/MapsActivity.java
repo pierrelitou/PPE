451e17badae229349729ee493a19578b202aa279
pierrelitou <pierre.litou@edu.ece.fr>
2019-03-18 15:02:51 +0100

    ras

--- FindYourSpot/FindYourSpot/app/src/main/java/fr/danielcc/myapplication3/MapsActivity.java
+++ FindYourSpot/FindYourSpot/app/src/main/java/fr/danielcc/myapplication3/MapsActivity.java
@@ -3,24 +3,22 @@ package fr.danielcc.myapplication3;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-
-import java.util.ArrayList;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 
