4b04dcd54b07361677e0eb0800d77ad984df38a4
jaguyard <jean-adrien.guyard@edu.ece.fr>
2019-03-30 12:48:05 +0100

    Retour de view events

--- FindYourSpot/FindYourSpot/app/src/main/java/fr/danielcc/findyourspot/Server.java
+++ FindYourSpot/FindYourSpot/app/src/main/java/fr/danielcc/findyourspot/Server.java
@@ -2,11 +2,12 @@ package fr.danielcc.findyourspot;
 
 public class Server {
 
-    public static final String URL = "http://192.168.0.14/findyourspot/";
+    public static final String URL = "http://192.168.1.40/findyourspot/";
 
     public static String firstname="";
     public static String lastname="";
     public static String dateofbirth="";
     public static String pseudo="";
     public static String photoprofil="";
+    public static String iduser="";
 }
